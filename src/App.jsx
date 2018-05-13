f1950eb63b6648357b421651a577b01a0f58d92c
ivan-cordoba <ivan.cordoba95@gmail.com>
2018-05-13 15:37:18 -0700

    Completed Level 1, added search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,51 @@
-var movies = [
-  {title: 'Mean Girls'},
-  {title: 'Hackers'},
-  {title: 'The Grey'},
-  {title: 'Sunshine'},
-  {title: 'Ex Machina'},
-];
-var App = () => {
-  return (
-    <div>
-      <table>
-        <tr><th>Movie List</th></tr>
-        {
-          movies.map(function(movie) {
-            return <tr><td>{movie.title}</td></tr>;
-          })
-        }
-      </table>    
-    </div>
-  );
-};
+class App extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.searchInput = '';
+    this.movies = [
+      {title: 'Mean Girls'},
+      {title: 'Hackers'},
+      {title: 'The Grey'},
+      {title: 'Sunshine'},
+      {title: 'Ex Machina'},
+    ];
+    this.state = {
+      movies: this.movies,
+      filterText: ''
+    };
+  }
+
+  updateInput(event) {
+    this.searchInput = event.target.value;
+  }
+
+  handleSearch() {
+    var filteredList = this.movies.filter(movie => movie.title.toLocaleLowerCase().includes(this.searchInput.toLocaleLowerCase()));
+    if (filteredList.length === 0) {
+      filteredList = [{title: 'No Results'}];
+    }
+    this.setState({
+      movies: filteredList
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Movies List</h2>
+        <input onChange={this.updateInput.bind(this)}/><button onClick={this.handleSearch.bind(this)}>Go</button>
+        <table><tbody>
+          <tr><th>Movies</th></tr>
+          {
+            this.state.movies.map(function(movie) {
+              return (<tr><td>{movie.title}</td></tr>);
+            })
+          }
+        </tbody></table>    
+      </div>
+    );
+  }
+}
+window.App = App;
 ReactDOM.render(<App/>, document.getElementById('app'));
