91df1554b15c92ee7cea9d87aa73d97191044498
ivan-cordoba <ivan.cordoba95@gmail.com>
2018-05-13 15:47:05 -0700

    Completed Level 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,26 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.searchInput = '';
-    this.movies = [
-      {title: 'Mean Girls'},
-      {title: 'Hackers'},
-      {title: 'The Grey'},
-      {title: 'Sunshine'},
-      {title: 'Ex Machina'},
-    ];
+    this.titleInput = '';
+    this.movies = [];
     this.state = {
       movies: this.movies,
       filterText: ''
     };
   }
 
+  displayAllMovies() {
+    this.setState({
+      movies: this.movies
+    });
+  }
+
   updateInput(event) {
     this.searchInput = event.target.value;
+  }
+
+  updateAddMovieInput(event) {
+    this.titleInput = event.target.value;
   }
 
   handleSearch() {
@@ -30,10 +35,17 @@ class App extends React.Component {
     });
   }
 
+  handleAddMovie() {
+    var newMovie = {title: this.titleInput};
+    this.movies.push(newMovie);
+    this.displayAllMovies();
+  }
+
   render() {
     return (
       <div>
         <h2>Movies List</h2>
+        <input onChange={this.updateAddMovieInput.bind(this)}/><button onClick={this.handleAddMovie.bind(this)}>Add</button>
         <input onChange={this.updateInput.bind(this)}/><button onClick={this.handleSearch.bind(this)}>Go</button>
         <table><tbody>
           <tr><th>Movies</th></tr>
